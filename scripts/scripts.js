3595107b62f98502fb654e8899ce3b49bbfab860
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-04 10:20:32 -0300

    Correção  menu mobile no desktop

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,10 +3,10 @@ let menuMobile = document.querySelector('#menu-mobile')
 let fecharMenu = document.querySelector('#fechar-menu')
 
 hamburgerMenu.addEventListener('click', function(){
-    menuMobile.classList.remove('fechar-menu')
+    menuMobile.classList.remove('d-none')
     menuMobile.classList.add('mostrar-menu')
 })
 fecharMenu.addEventListener('click', function(){
     menuMobile.classList.remove('mostrar-menu')
-    menuMobile.classList.add('fechar-menu')
+    menuMobile.classList.add('d-none')
 })
