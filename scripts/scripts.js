40f1cc0fb5b5b72bb1c53386e53598169acc6310
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-04 17:04:42 -0300

    Adiciona menu e submenu a barra mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,3 +17,18 @@ fecharMenu.addEventListener('click', function(){
     // remove animacao da barra de navegacao mobile
     menuMobileBarra.classList.remove('animacao-navegacao-mobile')
 })
+
+let itensMenuMobile = document.querySelectorAll('.barra-menu-mobile .item-menu')
+// i = 1 PARA PULAR O PRIMEIRO ELEMENTO
+for (let i = 1; i < itensMenuMobile.length; i++){
+    itensMenuMobile[i].addEventListener('click', function(event){
+        // modifica visibilidade do submenu
+        // pega o segundo elemento (ul)
+        if(this.children[1].getAttribute('class')){
+            this.children[1].classList.remove('d-none')
+        }else{
+            this.children[1].classList.add('d-none')
+        }
+        
+    })
+}
