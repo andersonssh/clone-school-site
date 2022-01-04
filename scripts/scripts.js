518494346ca77922e3b7455bdd396c97845b62f4
andersonssh <4nd3r50n4r4uj0@gmail.com>
2022-01-04 15:05:42 -0300

    Adiciona animacao  menu mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,19 @@
 let hamburgerMenu = document.querySelector('#hamburger')
 let menuMobile = document.querySelector('#menu-mobile')
+let menuMobileBarra = document.querySelector('#barra-navegacao-mobile')
 let fecharMenu = document.querySelector('#fechar-menu')
 
 hamburgerMenu.addEventListener('click', function(){
-    menuMobile.classList.remove('d-none')
+    menuMobile.classList.remove('esconder-menu')
     menuMobile.classList.add('mostrar-menu')
+
+    // adiciona animacao da barra de navegacao mobile
+    menuMobileBarra.classList.add('animacao-navegacao-mobile')
 })
 fecharMenu.addEventListener('click', function(){
     menuMobile.classList.remove('mostrar-menu')
-    menuMobile.classList.add('d-none')
+    menuMobile.classList.add('esconder-menu')
+
+    // remove animacao da barra de navegacao mobile
+    menuMobileBarra.classList.remove('animacao-navegacao-mobile')
 })
